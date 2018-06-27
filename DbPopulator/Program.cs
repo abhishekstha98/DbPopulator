96bb2e577c117351646eb0a1ed799c647b1cda2c
Abhishek Krishna Shrestha <abhishek.stha17@gmail.com>
2018-06-27 12:20:58 +0545

    update
    storing the phone number as string in database
    and changes in line 73 and 74 because of indexoutofrange exception previously

--- DbPopulator/Program.cs
+++ DbPopulator/Program.cs
@@ -15,12 +15,16 @@ namespace DbPopulator
             int row = int.Parse(Console.ReadLine());
             Random rnd = new Random();
             int no_row=0;
+            
+            //names, emailid, designation
             List<String> names = new List<string> { "Matt", "Jimmy", "Hyman", "Lane", "Morris", "Garry", "Randall", "Sydney", "Jefferey", "Ruben", "Jacelyn", "Keena", "Cecile", "Jazmin", "Joaquina", "Hyun", "Twana", "Luz", "Tari", "Rena", "Aaron", "Abdul", "Abe", "Abel", "Abraham", "Abram", "Adalberto", "Adam", "Adan", "Adolfo ", "Adolph", "Adrian", "Agustin", "Ahmad", "Ahmed", "Al", "Alan", "Albert", "Alberto", "Alden", "Aldo", "Alec", "Alejandro", "Alex", "Alexander", "Alexis", "Alfonso", "Alfonzo", "Alfred ", "Barney", "Barrett", "Barry", "Bart", "Barton", "Basil", "Beau", "Ben", "Benedict", "Benito", "Benjamin", "Bennett", "Bennie", "Benny", "Benton", "Bernard", "Bernardo", "Bernie", "Berry", "Bert", "Bertram", "Bill", "Billie", "Billy", "Caleb", "Calvin", "Cameron", "Carey", "Carl", "Carlo", "Carlos", "Carlton", "Carmelo", "Carmen", "Carmine", "Carol", "Carrol", "Carroll", "Carson", "Carter", "Cary", "Casey", "Cecil", "Cedric", "Cedrick", "Cesar", "Chad", "Chadwick", "Chance", "Chang", "Charles", "Charley", "Charlie" };
             List<String> surname = new List<string> { "King", "Morgan", "Kline", "Beck", "Gilbert", "Mcclure", "Lang", "Bolton", "Brown", "Hatfield", "Newton", "Bernard", "Short", "Hunter", "Jenkins", "Aguilar", "Riley", "Riggs", "Collins", "Valdez" };
             List<String> domain = new List<string> { "@gmail.com", "@yahoo.com", "@hotmail.com" };
             List<String> designation = new List<string> { ".NET DEVELOPER", "QA", "DBA", "DESIGNING", "SECURITY" };
             List<String> n_names = new List<string>();
             List<String> email_id = new List<string>();
+            
+            //connection string for database access
             String cs = @"Server=DESKTOP-EOG8KD1\ADMIN;Database=Employee;User Id=sa;Password = webuser; ";
             SqlConnection sc = new SqlConnection(cs);
             try
@@ -32,7 +36,7 @@ namespace DbPopulator
                 {
                     while (reader.Read())
                     {
-                        no_row = reader.GetInt32(0);
+                        no_row = reader.GetInt32(0);//gets the number of rows already present in database
                         Console.WriteLine(no_row);
                     }
                 }
@@ -44,10 +48,12 @@ namespace DbPopulator
             finally
             {
                 Console.ReadLine();
-                sc.Close();
+                sc.Close();//close is must 
             }
             for (int i = 0; i < row; i++){
+                //generating random names by randomly selecting names and sunrames and adding them together to form a full name
                 n_names.Add(names[(rnd.Next(no_row,no_row+row)%rnd.Next(1,102))] + " " + surname[(rnd.Next(35, 54)) % (19)]);
+                //using the names and randomly generated numbers along with different domain names to generate unique ids
                 email_id.Add(n_names[i] + rnd.Next(1234,9999) + domain[rnd.Next(3)]);
                 email_id[i] = email_id[i].Replace(' ', '_');
             }
@@ -64,33 +70,13 @@ namespace DbPopulator
                     {
                         Console.WriteLine(i);
                         cmd.Parameters.AddWithValue("@ID",i );
-                        cmd.Parameters.AddWithValue("@NAME", n_names[(i%49)]);
-                        cmd.Parameters.AddWithValue("@EMAILID", email_id[(i%49)]);
+                        cmd.Parameters.AddWithValue("@NAME", n_names[(i%row)]);
+                        cmd.Parameters.AddWithValue("@EMAILID", email_id[(i%row)]);
                         cmd.Parameters.AddWithValue("@DESIGN", designation[rnd.Next(5)]);
-                        cmd.Parameters.AddWithValue("@PHONE", Convert.ToInt64("9841" + (rnd.Next(110000, 999999)).ToString()));
+                        //generating phone numbers
+                        cmd.Parameters.AddWithValue("@PHONE", "9841" + rnd.Next(110000, 999999).ToString()));
                         cmd.ExecuteNonQuery();
                     }
-                    /*SELECT COMMAND
-                     * the following is select query
-                     * 
-                     *
-                    SqlCommand command = new SqlCommand("SELECT * FROM EMPLOYEETBL", sc);
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetInt32(0),
-                                reader.GetString(1),
-                                reader.GetString(2),
-                                reader.GetString(3), reader.GetInt64(4));
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No rows found.");
-                    }*/
                 }
                 catch (Exception e)
                 {
